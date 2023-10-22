b47b6641588da106cd978459a49982017b0a9640
LyubomirT <terlyubomir@gmail.com>
2023-10-22 11:47:05 -0600

    Now you can create your own champions!

--- main.py
+++ main.py
@@ -4,13 +4,10 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import os
-import subprocess
-import shutil
-import time
 import random
 import requests
 from dotenv import load_dotenv
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import json
 
 load_dotenv()
@@ -57,6 +54,11 @@ def load_user_data():
             for uid in users:
                 users[uid]["id"] = int(uid)
             
+            # Convert all champion UIDs to integers
+            for uid in users:
+                for champion in users[uid]["champions"]:
+                    champion["id"] = int(champion["id"])
+            
             # Create the appropriate User objects
             for uid in users:
                 user = users[uid]
@@ -73,6 +75,7 @@ def load_user_data():
 load_user_data()
 
 def save_user_data():
+    global users
     # Prepare the users dictionary for serialization
     users_serializable = {}
     for user in users.values():
@@ -84,9 +87,14 @@ def save_user_data():
             "email": user.email,
             "champions": user.champions
         }
-        print("User:", user.username)
     with open(STORAGE_TARGET_DIR, "w") as f:
         f.write(json.dumps(users_serializable))
+    # Update the global users dictionary
+    users = users_serializable
+
+
+
+
 
 @app.route('/')
 def index():
@@ -137,10 +145,71 @@ def chat_back():
     # Return the response text
     return response_text
 
+"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AI vs AI - Ultimate Chatbot Battles | Create</title>
+        <link rel="stylesheet" href="{{url_for('static', filename='css/create.css')}}">
+    </head>
+    <body>
+        <header>
+            <h1>AI vs AI - Ultimate Chatbot Battles</h1>
+        </header>
+        <main>
+            <section class="create">
+                <h2>Create your champion</h2>
+                <form action="{{url_for('create')}}" method="POST">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Enter your champion's name" required>
+                    <label for="description">Description</label>
+                    <textarea name="description" id="description" cols="30" rows="10" placeholder="Enter your champion's description" required maxlength="500"></textarea>
+                    <label for="instructions">Instructions</label>
+                    <textarea name="instructions" id="instructions" cols="30" rows="10" placeholder="Enter your champion's instructions" required maxlength="6000"></textarea>
+                    <label for="greeting">Greeting</label>
+                    <textarea name="greeting" id="greeting" cols="30" rows="10" placeholder="Enter your champion's greeting" required maxlength="500"></textarea>
+                    <input type="submit" value="Create">
+                </form>
+            </section>
+        </main>
+        <footer>
+            <p>AI vs AI is a project maintained by <a href="https://github.com/LyubomirT">LyubomirT</a>.</p>
+        </footer>
+    </body>
+</html>
+"""
+
 @app.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
-    return render_template('create.html')
+    if request.method == 'POST':
+        # Get the form data
+        name = request.form['name']
+        description = request.form['description']
+        instructions = request.form['instructions']
+        greeting = request.form['greeting']
+
+        # Create the champion
+        champion = {
+            "id": random.randint(1, 1000000),
+            "name": name,
+            "description": description,
+            "instructions": instructions,
+            "greeting": greeting
+        }
+
+        # Add the champion to the user's list of champions
+        current_user.champions.append(champion)
+
+        # Save the user data
+        save_user_data()
+
+        # Redirect to the user's profile page
+        return redirect(url_for('profile', username=current_user.username))
+
+    else:
+        return render_template('create.html')
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
